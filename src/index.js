b3ae88f00e7d91958c08febbfb64c54297e683ca
Prajjwal-5432 <prajjwalsahu5432@gmail.com>
2021-09-25 19:19:12 +0530

    Sending messages to rooms

--- src/index.js
+++ src/index.js
@@ -27,30 +27,32 @@ io.on('connection', socket => {
 
         socket.join(user.room)
 
-        socket.emit('message', generateMessage('Welcome'))
-        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined`))
+        socket.emit('message', generateMessage('Admin', 'Welcome'))
+        socket.broadcast.to(user.room).emit('message', generateMessage(user.username, `${user.username} has joined`))
         callback()
     })
 
     socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
         const filter = new Filter()
 
         if (filter.isProfane(message)) {
             return callback('profanity is not allowed')
         }
-        io.to('india').emit('message', generateMessage(message))
+        io.to(user.room).emit('message', generateMessage(user.username, message))
         callback('The message was delivered')
     })
 
     socket.on('disconnect', () => {
         const user = removeUser(socket.id)
-        if(user) {
-            io.to(user.room).emit('message', generateMessage(`${user.username} has left`))
+        if (user) {
+            io.to(user.room).emit('message', generateMessage(user.username, `${user.username} has left`))
         }
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage', locationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', locationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 })
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,11 +1,13 @@
-const generateMessage = text => {
+const generateMessage = (username, text) => {
     return {
+        username,
         text,
         createdAt: new Date().getTime()
     }
 }
-const locationMessage = url => {
+const locationMessage = (username, url) => {
     return {
+        username,
         url,
         createdAt: new Date().getTime()
     }
