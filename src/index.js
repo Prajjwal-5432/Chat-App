3a391ea5e5bf2983b0a8796f4370de150d901840
Prajjwal-5432 <prajjwalsahu5432@gmail.com>
2021-09-25 14:54:03 +0530

    Rendering location messages

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ io.on('connection', socket => {
     socket.on('disconnect', () => io.emit('message', 'A user has left'))
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
         callback()
     })
 })
