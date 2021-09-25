0d14336509ede3982a406c2ef6b6eb9aad43454d
Prajjwal-5432 <prajjwalsahu5432@gmail.com>
2021-09-25 15:12:57 +0530

    Working with time

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const { generateMessage } = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -16,18 +17,20 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', socket => {
     console.log('New Web socket connection')
 
-    socket.emit('message', 'Welcome')
-    socket.broadcast.emit('message', 'A new user has joined')
+    socket.emit('message', generateMessage('Welcome'))
+    socket.broadcast.emit('message', generateMessage('A new user has joined'))
+
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
 
         if (filter.isProfane(message)) {
             return callback('profanity is not allowed')
         }
-        io.emit('message', message)
+        io.emit('message', generateMessage(message))
         callback('The message was delivered')
     })
-    socket.on('disconnect', () => io.emit('message', 'A user has left'))
+
+    socket.on('disconnect', () => io.emit('message', generateMessage('A user has left')))
 
     socket.on('sendLocation', (coords, callback) => {
         io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
